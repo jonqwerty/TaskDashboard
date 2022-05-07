36f651304d0cb0501729d2e475c7fb080d7bfb4a
Serhii B <s_blyshchak@ukr.net>
2022-05-08 01:13:10 +0300

    Finish drag and drop functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,42 +4,42 @@ import "./App.css"
 import InputField from "./components/InputField"
 import { Todo } from "./model"
 import TodoList from "./components/TodoList"
-import { DragDropContext } from "react-beautiful-dnd"
+import { DragDropContext, DropResult } from "react-beautiful-dnd"
 
-let name: string
-let age: number
-let house: number | string
+// let name: string
+// let age: number
+// let house: number | string
 
-let isStudent: boolean
-let hobbis: string[]
-let role: [number, string]
+// let isStudent: boolean
+// let hobbis: string[]
+// let role: [number, string]
 
-let country: any
-let city: unknown
+// let country: any
+// let city: unknown
 
-type Person = {
-  name: "string"
-  age?: "number"
-}
+// type Person = {
+//   name: "string"
+//   age?: "number"
+// }
 
-let printName: (name: string) => void // returns undefined
-let printHome: (name: string) => never // returns nothing
+// let printName: (name: string) => void // returns undefined
+// let printHome: (name: string) => never // returns nothing
 
-let person: Person
+// let person: Person
 
-let listOfPersons: Person[]
+// let listOfPersons: Person[]
 
-//  type  vs interface
+// //  type  vs interface
 
-type Man = {
-  name: "string"
-  age?: "number"
-}
+// type Man = {
+//   name: "string"
+//   age?: "number"
+// }
 
-interface Woman {
-  name: "string"
-  age?: "number"
-}
+// interface Woman {
+//   name: "string"
+//   age?: "number"
+// }
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("")
@@ -55,8 +55,44 @@ const App: React.FC = () => {
     }
   }
 
+  const onDragEnd = (result: DropResult) => {
+    const { destination, source } = result
+
+    console.log(result)
+
+    if (!destination) {
+      return
+    }
+
+    if (destination.droppableId === source.droppableId && destination.index === source.index) {
+      return
+    }
+
+    let add
+    let active = todos
+    let complete = completedTodos
+    // Source Logic
+    if (source.droppableId === "TodosList") {
+      add = active[source.index]
+      active.splice(source.index, 1)
+    } else {
+      add = complete[source.index]
+      complete.splice(source.index, 1)
+    }
+
+    // Destination Logic
+    if (destination.droppableId === "TodosList") {
+      active.splice(destination.index, 0, add)
+    } else {
+      complete.splice(destination.index, 0, add)
+    }
+
+    setCompletedTodos(complete)
+    setTodos(active)
+  }
+
   return (
-    <DragDropContext onDragEnd={() => {}}>
+    <DragDropContext onDragEnd={onDragEnd}>
       <div className="App">
         <span className="heading">Task dashboard</span>
         <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
