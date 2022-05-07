f46b98478513de50e8e75476e8eefed89ecffb70
Serhii B <s_blyshchak@ukr.net>
2022-05-08 00:47:41 +0300

    Partly implement drag and drop, remove strict mode

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css"
 import InputField from "./components/InputField"
 import { Todo } from "./model"
 import TodoList from "./components/TodoList"
+import { DragDropContext } from "react-beautiful-dnd"
 
 let name: string
 let age: number
@@ -44,6 +45,8 @@ const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("")
   const [todos, setTodos] = useState<Todo[]>([])
 
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([])
+
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault()
     if (todo) {
@@ -53,12 +56,19 @@ const App: React.FC = () => {
   }
 
   return (
-    <div className="App">
-      <span className="heading">Task dashboard</span>
-      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+    <DragDropContext onDragEnd={() => {}}>
+      <div className="App">
+        <span className="heading">Task dashboard</span>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
 
-      <TodoList todos={todos} setTodos={setTodos} />
-    </div>
+        <TodoList
+          todos={todos}
+          setTodos={setTodos}
+          completedTodos={completedTodos}
+          setCompletedTodos={setCompletedTodos}
+        />
+      </div>
+    </DragDropContext>
   )
 }
 
