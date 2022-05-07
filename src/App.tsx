e7443600134229e9eb0e10e6ad5fc378d802f738
Serhii B <s_blyshchak@ukr.net>
2022-05-07 14:14:09 +0300

    Implement adding todo object

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import logo from "./logo.svg"
 import "./App.css"
 import InputField from "./components/InputField"
+import { Todo } from "./model"
 
 let name: string
 let age: number
@@ -40,11 +41,20 @@ interface Woman {
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("")
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }])
+      setTodo("")
+    }
+  }
 
   return (
     <div className="App">
       <span className="heading">Task dashboard</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   )
 }
